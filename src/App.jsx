a9d42f4b778e54da18162b40ecf23fc1336237b6
OleksandrPinchuk <hohhua@gmail.com>
2024-08-24 14:45:43 +0200

    copied files

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-import ContactForm from './components/ContactForm/ContactForm'
-import ContactList from './components/ContactList/ContactList'
-import SearchBox from './components/SearchBox/SearchBox'
-// import initialContacts from './initialContacts.json'
-import css from './App.module.css'
-
-// const allContacts = () => {
-//   const storedContacts = localStorage.getItem('contacts');
-//   return storedContacts ? JSON.parse(storedContacts) : initialContacts;
-// };
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import css from './App.module.css';
 
 function App() {
-
-  // const [contacts, setContacts] = useState(allContacts);
-  // const [searchName, setSearchName] = useState('');
-  
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts))
-  // }, [contacts]);
-  
-  // const addContact = (newContact) => {
-  //   setContacts([...contacts, newContact])
-  // }
-
-  // const deleteContact = (contactId) => {
-  //     setContacts((contacts) =>
-  //     contacts.filter((contact) => contact.id !== contactId)
-  //   );
-  // }
-
-  // const handleSearch = (value) => { setSearchName(value) };
-
-  // const filteredContacts = contacts.filter((contact) => 
-  //   contact.name.toLowerCase().includes(searchName.toLowerCase())
-  // );
 
   return (
 
@@ -43,6 +14,6 @@ function App() {
         <ContactList />
       </div>
   )
-}
+};
 
-export default App
+export default App;
